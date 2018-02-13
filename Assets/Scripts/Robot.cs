895ba7a55636a699930f5dc313603f208aad1be1
5141 <deviola08@gmail.com>
2018-02-13 16:17:02 -0600

    Changed and added some stuff to start work on placeholders

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -45,132 +45,132 @@ public class Robot : MonoBehaviour {
 		originalBackboardX = hugBackboard.transform.localPosition.x;
 		startMeasure = transform.position;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		float z = Input.GetAxis("Vertical");
 		float x = Input.GetAxis ("Horizontal");
 		float left = 0;
 		float right = 0;
-		float speed = 10;
+		float speed = 15;
 		if (player.robotControl) {
 			left = Mathf.Clamp (z + x, -1.0f, 1.0f)*speed;
 			right = Mathf.Clamp (z - x, -1.0f, 1.0f)*speed;
 
-			double railSpeed = 0.1;
+			double railSpeed = 0.3;
 			if (Input.GetKey (KeyCode.Keypad1)) {
 				if (upperRail.transform.localPosition.y > upperRailMinY) {
-					
-					upperRail.transform.localPosition = 
+
+					upperRail.transform.localPosition =
 						new Vector3 (upperRail.transform.localPosition.x,
 							(float)(upperRail.transform.localPosition.y - railSpeed),
 							upperRail.transform.localPosition.z);
-					
+
 				}else if (midRail.transform.localPosition.y > midRailMinY) {
-					
-					midRail.transform.localPosition = 
+
+					midRail.transform.localPosition =
 					new Vector3 (midRail.transform.localPosition.x,
 							(float)(midRail.transform.localPosition.y - railSpeed),
 						midRail.transform.localPosition.z);
-					
+
 				}
 			} else if (Input.GetKey (KeyCode.Keypad4)) {
 				if (midRail.transform.localPosition.y < midRailMinY + midRailMaxYIncrease) {
-					
-					midRail.transform.localPosition = 
+
+					midRail.transform.localPosition =
 					new Vector3 (midRail.transform.localPosition.x,
 						(float)(midRail.transform.localPosition.y + railSpeed),
 						midRail.transform.localPosition.z);
-					
+
 				} else if (upperRail.transform.localPosition.y < upperRailMinY + upperRailMaxYIncrease) {
-					
-					upperRail.transform.localPosition = 
+
+					upperRail.transform.localPosition =
 						new Vector3 (upperRail.transform.localPosition.x,
 							(float)(upperRail.transform.localPosition.y + railSpeed),
 							upperRail.transform.localPosition.z);
-					
+
 				}
 			}
 
-			double hugSpeed = 0.1;
+			double hugSpeed = 0.3;
 			if (Input.GetKey (KeyCode.Keypad2) && hug.transform.localPosition.y > hugMinY) {
-				
-				hug.transform.localPosition = 
+
+				hug.transform.localPosition =
 					new Vector3 (hug.transform.localPosition.x,
 						(float)(hug.transform.localPosition.y - hugSpeed),
 						hug.transform.localPosition.z);
-				
+
 			} else if (Input.GetKey (KeyCode.Keypad5) && hug.transform.localPosition.y < hugMinY + hugMaxYIncrease) {
 
-				hug.transform.localPosition = 
+				hug.transform.localPosition =
 					new Vector3 (hug.transform.localPosition.x,
 						(float)(hug.transform.localPosition.y + hugSpeed),
 						hug.transform.localPosition.z);
-				
+
 			}
 
 			if (Input.GetKey (KeyCode.Keypad0)) {
-				
+
 				hugBase.transform.localPosition =
 					new Vector3 (baseXRetracted,
 						hugBase.transform.localPosition.y,
 						hugBase.transform.localPosition.z);
-				
+
 			} else {
-				
+
 				hugBase.transform.localPosition =
 					new Vector3 (originalBaseX,
 						hugBase.transform.localPosition.y,
 						hugBase.transform.localPosition.z);
-				
+
 			}
 			if (Input.GetKey (KeyCode.KeypadEnter)) {
 				startMeasure = transform.position;
 			}
 			if (Input.GetKey (KeyCode.KeypadPeriod)) {
-				
-				hugRightFinger.transform.localPosition = 
-					new Vector3 (hugRightFinger.transform.localPosition.x,	
+
+				hugRightFinger.transform.localPosition =
+					new Vector3 (hugRightFinger.transform.localPosition.x,
 					hugRightFinger.transform.localPosition.y,
 					(float)(originalRightFingerZ - fingerOffset));
-				
-				hugLeftFinger.transform.localPosition = 
-					new Vector3 (hugRightFinger.transform.localPosition.x,	
+
+				hugLeftFinger.transform.localPosition =
+					new Vector3 (hugRightFinger.transform.localPosition.x,
 					hugRightFinger.transform.localPosition.y,
 					(float)(originalLeftFingerZ + fingerOffset));
-				
+
 			} else {
-				
-				hugRightFinger.transform.localPosition = 
-					new Vector3 (hugRightFinger.transform.localPosition.x,	
+
+				hugRightFinger.transform.localPosition =
+					new Vector3 (hugRightFinger.transform.localPosition.x,
 					hugRightFinger.transform.localPosition.y,
 					originalRightFingerZ);
-				
-				hugLeftFinger.transform.localPosition = 
-					new Vector3 (hugRightFinger.transform.localPosition.x,	
+
+				hugLeftFinger.transform.localPosition =
+					new Vector3 (hugRightFinger.transform.localPosition.x,
 					hugRightFinger.transform.localPosition.y,
 					originalLeftFingerZ);
-				
+
 			}
 
 			float backboardSpeed = 0.01f;
 			if (Input.GetKey (KeyCode.Keypad3) && hugBackboard.transform.localPosition.x > originalBackboardX) {
-				
-				hugBackboard.transform.localPosition = 
+
+				hugBackboard.transform.localPosition =
 					new Vector3 (hugBackboard.transform.localPosition.x - backboardSpeed,
 						hugBackboard.transform.localPosition.y,
 						hugBackboard.transform.localPosition.z);
-				
+
 			} else if (Input.GetKey (KeyCode.Keypad6) && hugBackboard.transform.localPosition.x < originalBackboardX + backboardMaxXIncrease) {
 
-				hugBackboard.transform.localPosition = 
+				hugBackboard.transform.localPosition =
 					new Vector3 (hugBackboard.transform.localPosition.x + backboardSpeed,
 						hugBackboard.transform.localPosition.y,
 						hugBackboard.transform.localPosition.z);
-				
+
 			}
 		}
-		
+
 
 		for (int i = 0; i < leftWheels.Length; i++) {
 			leftWheels [i].motorTorque = left;
